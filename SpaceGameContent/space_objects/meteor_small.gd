831261e6969f650c19f4ad0f2ec69bea661badf8
EakyRtk <eakyrtkysf@gmail.com>
2025-03-22 01:24:57 +0300

    update

--- SpaceGameContent/space_objects/meteor_small.gd
+++ SpaceGameContent/space_objects/meteor_small.gd
@@ -5,23 +5,42 @@ extends Area2D
 @export var drift_speed : float = 100.0
 @export var damage : int = 1
 
+@onready var hit_sfx : AudioStreamPlayer2D = $Hit
+
 @export var health : int = 2:
-    set(value):
-        if value <= 0: queue_free()
-        health = value
-        health_bar.value = health
+	set(value):
+		#can 0 ise yok edeceğiz
+		if value <= 0:
+
+			#-- görünürlüğü kapadık, hareket etmesini durdurmak için processi kapadık ve collision disabledi true yaptık ki artık hesaplamaya katılmasın
+			#ancak sfx in bitmesini bekliyoruz. Böylece SFX yarıda kesilmeyecek.
+			
+			#direkt visible = false veya hide() dersek _screen_exited çalışacak ve sfx bitmeden obje yok olacak
+			$Line2D.hide()
+			$Sprite2D.hide()
+			$HealthBrogressBar.hide()
+
+			set_process(false)
+			$CollisionShape2D.set_deferred("disabled", true)
+			#----
+			await hit_sfx.finished
+			queue_free()
+		health = value
+		health_bar.value = health
 
 func _ready() -> void:
-    health_bar.max_value = health
-    health_bar.value = health
+	health_bar.max_value = health
+	health_bar.value = health
 
 func _process(delta: float) -> void:
-    position.x -= drift_speed * delta
+	position.x -= drift_speed * delta
 
 func _screen_exited() -> void:
-    queue_free()
+	queue_free()
 
 func _on_area_entered(area:Area2D) -> void:
-    if area is Bullet:
-        health -= area.damage
-        area.queue_free()
+	if area is Bullet:
+		area.queue_free()
+		hit_sfx.play()
+
+		health -= area.damage
